066335c193b9419396d08b9bcfad634666c6f7b8
Raymond Chen <ahrchen@gmail.com>
2022-03-28 17:38:44 -0700

    Add map and location

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,12 +5,66 @@
 //  Created by Raymond Chen on 3/24/22.
 //
 
+import MapKit
 import SwiftUI
 
+
 struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        Text(location.name)
+                            .fixedSize()
+                    }
+                    .onTapGesture {
+                        viewModel.selectedPlace = location
+                    }
+                }
+            }
+                .ignoresSafeArea()
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: viewModel.mapRegion.center.latitude, longitude: viewModel.mapRegion.center.longitude)
+                        viewModel.locations.append(newLocation)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                    .padding(.bottom)
+                }
+            }
+            
+        }
+        .sheet(item: $viewModel.selectedPlace) { place in
+            EditView(location: place) { newLocation in
+                if let index = viewModel.locations.firstIndex(of: place) {
+                    viewModel.locations[index] = newLocation
+                }
+            }
+        }
     }
 }
 
